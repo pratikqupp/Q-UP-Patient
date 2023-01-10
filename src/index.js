dbcca32a3ac90c7ea00f02a19e6e1a92fea5816d
Pratik Pansare <pratik.p@qupapp.com>
2023-01-10 16:12:40 +0530

    iiiii

--- src/index.js
+++ src/index.js
@@ -1,28 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import "./style.css";
-import SignUp from "./views/sign-up";
-import VerifyOTP from "./views/verify-o-t-p";
-import BookingScreen from "./views/booking-screen";
-import Login from "./views/login";
-import Home from "./views/home";
-import GenralCheckup from "./views/genral-checkup";
+import './style.css'
+import Frame1 from './views/frame1'
 
 const App = () => {
   return (
     <Router>
       <div>
-        <Route component={SignUp} exact path="/sign-up" />
-        <Route component={VerifyOTP} exact path="/verify-o-t-p" />
-        <Route component={BookingScreen} exact path="/booking-screen" />
-        <Route component={GenralCheckup} exact path="/genral-checkup" />
-        <Route component={Login} exact path="/login" />
-        <Route component={Home} exact path="/:drName/:drId/:specality" />
+        <Route component={Frame1} exact path="/" />
       </div>
     </Router>
-  );
-};
+  )
+}
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ src/views/frame1.js
@@ -0,0 +1,377 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './frame1.css'
+
+const Frame1 = (props) => {
+
+
+  
+  return (
+    <div className="frame1-container">
+      <Helmet>
+        <title>Appointment</title>
+      </Helmet>
+      <div className="frame1-frame1">
+        <img
+          alt="Rectangle69012"
+          src="/playground_assets/rectangle69012-h65-400w.png"
+          className="frame1-rectangle690"
+        />
+        <img
+          alt="Rectangle69113"
+          src="/playground_assets/rectangle69113-c684-200h.png"
+          className="frame1-rectangle691"
+        />
+        <img
+          alt="Vector22414"
+          src="/playground_assets/vector22414-k4c.svg"
+          className="frame1-vector224"
+        />
+        <span className="frame1-text">
+          <span>Appointment</span>
+        </span>
+        <img
+          alt="Rectangle69216"
+          src="/playground_assets/rectangle69216-51nn-400w.png"
+          className="frame1-rectangle692"
+        />
+        <img
+          alt="Rectangle69317"
+          src="/playground_assets/rectangle69317-luor-200h.png"
+          className="frame1-rectangle693"
+        />
+        <div className="frame1-group1795">
+          <div className="frame1-group1373">
+            <span className="frame1-text02">
+              <span>Prepaid Booking</span>
+            </span>
+          </div>
+        </div>
+        <div className="frame1-group1806">
+          <div className="frame1-group13731">
+            <span className="frame1-text04">
+              <span>Prepaid Booking</span>
+            </span>
+          </div>
+        </div>
+        <span className="frame1-text06">
+          <span>Booking Rules</span>
+        </span>
+        <div className="frame1-group1797">
+          <div className="frame1-group522">
+            <img
+              alt="Ellipse48119"
+              src="/playground_assets/ellipse48119-4lgm-200h.png"
+              className="frame1-ellipse48"
+            />
+            <span className="frame1-text08">
+              <span>Booking Full</span>
+            </span>
+          </div>
+        </div>
+        <img
+          alt="Line81121"
+          src="/playground_assets/line81121-jdd.svg"
+          className="frame1-line81"
+        />
+        <img
+          alt="Line82122"
+          src="/playground_assets/line82122-3k4f.svg"
+          className="frame1-line82"
+        />
+        <img
+          alt="Line83123"
+          src="/playground_assets/line83123-029n.svg"
+          className="frame1-line83"
+        />
+        <div className="frame1-group1798">
+          <span className="frame1-text10">
+            <span>06:00 PM - 09:00 PM</span>
+          </span>
+        </div>
+        <div className="frame1-group1799">
+          <div className="frame1-group1364"></div>
+          <img
+            alt="Rectangle448129"
+            src="/playground_assets/rectangle448129-8uwi-200h.png"
+            className="frame1-rectangle448"
+          />
+        </div>
+        <div className="frame1-group1800">
+          <div className="frame1-group13641"></div>
+          <img
+            alt="Rectangle448133"
+            src="/playground_assets/rectangle448133-y5jf-200h.png"
+            className="frame1-rectangle4481"
+          />
+        </div>
+        <span className="frame1-text12">
+          <span>11:00 AM - 01:00 PM</span>
+        </span>
+        <span className="frame1-text14">
+          <span>11:00 AM - 01:00 PM</span>
+        </span>
+        <div className="frame1-group1801">
+          <div className="frame1-group5221">
+            <img
+              alt="Ellipse48138"
+              src="/playground_assets/ellipse48138-6h1r-200h.png"
+              className="frame1-ellipse481"
+            />
+            <span className="frame1-text16">
+              <span>Open</span>
+            </span>
+          </div>
+        </div>
+        <div className="frame1-group1802">
+          <div className="frame1-group5222">
+            <img
+              alt="Ellipse48142"
+              src="/playground_assets/ellipse48142-nio-200h.png"
+              className="frame1-ellipse482"
+            />
+            <span className="frame1-text18">
+              <span>Open</span>
+            </span>
+          </div>
+        </div>
+        <span className="frame1-text20">
+          <span>Morning</span>
+        </span>
+        <span className="frame1-text22">
+          <span>Evening</span>
+        </span>
+        <span className="frame1-text24">
+          <span>Afternoon</span>
+        </span>
+        <span className="frame1-text26">
+          <span>OPD</span>
+        </span>
+        <div className="frame1-group1803">
+          <img
+            alt="Vector195149"
+            src="/playground_assets/vector195149-dkhsr.svg"
+            className="frame1-vector195"
+          />
+          <img
+            alt="Rectangle632150"
+            src="/playground_assets/rectangle632150-w7mb.svg"
+            className="frame1-rectangle632"
+          />
+        </div>
+        <span className="frame1-text28">
+          <span>Medical representative queue</span>
+        </span>
+        <img
+          alt="Vector225152"
+          src="/playground_assets/vector225152-99if.svg"
+          className="frame1-vector225"
+        />
+        <div className="frame1-group1757">
+          <img
+            alt="Vector195154"
+            src="/playground_assets/vector195154-qr3v.svg"
+            className="frame1-vector1951"
+          />
+          <img
+            alt="Rectangle632155"
+            src="/playground_assets/rectangle632155-8jyj.svg"
+            className="frame1-rectangle6321"
+          />
+        </div>
+        <img
+          alt="Vector156"
+          src="/playground_assets/vector156-rj9i.svg"
+          className="frame1-vector"
+        />
+        <span className="frame1-text30">
+          <span>
+            <span>Thu</span>
+            <br></br>
+            <span>
+              15 Nov
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+          </span>
+        </span>
+        <span className="frame1-text35">
+          <span>
+            <span>
+              Wed
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <br></br>
+            <span>
+              14 Nov
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+          </span>
+        </span>
+        <span className="frame1-text40">
+          <span>
+            <span>
+              Today
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <br></br>
+            <span>12 Nov</span>
+          </span>
+        </span>
+        <span className="frame1-text45">
+          <span>
+            <span>Tomorrow</span>
+            <br></br>
+            <span>
+              13 Nov
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+          </span>
+        </span>
+        <img
+          alt="Rectangle694161"
+          src="/playground_assets/rectangle694161-4i8h-200h.png"
+          className="frame1-rectangle694"
+        />
+        <span className="frame1-text50">
+          <span>Select queue to join</span>
+        </span>
+        <img
+          alt="Vector226163"
+          src="/playground_assets/vector226163-bdt8.svg"
+          className="frame1-vector226"
+        />
+        <img
+          alt="Vector227164"
+          src="/playground_assets/vector227164-5a7.svg"
+          className="frame1-vector227"
+        />
+        <img
+          alt="Vector228165"
+          src="/playground_assets/vector228165-4r7.svg"
+          className="frame1-vector228"
+        />
+        <img
+          alt="Rectangle695166"
+          src="/playground_assets/rectangle695166-ejj9-200h.png"
+          className="frame1-rectangle695"
+        />
+        <span className="frame1-text52">
+          <span>This is Prepaid Booking Queue ₹ = 500 Rs</span>
+        </span>
+        <img
+          alt="Rectangle464168"
+          src="/playground_assets/rectangle464168-lw2-200h.png"
+          className="frame1-rectangle464"
+        />
+        <span className="frame1-text54">
+          <span>Skin Clinic Pune Clinic</span>
+        </span>
+        <span className="frame1-text56">
+          <span>MBBA, DCA, PGDM (Dermetologist)</span>
+        </span>
+        <span className="frame1-text58">
+          <span>Dr. Anil Patki</span>
+        </span>
+        <img
+          alt="Ellipse59172"
+          src="/playground_assets/ellipse59172-zo2u-200h.png"
+          className="frame1-ellipse59"
+        />
+        <div className="frame1-group1405">
+          <div className="frame1-group280">
+            <div className="frame1-group591">
+              <span className="frame1-text60">
+                <span>19874</span>
+              </span>
+              <img
+                alt="Vector178"
+                src="/playground_assets/vector178-91o.svg"
+                className="frame1-vector1"
+              />
+            </div>
+          </div>
+        </div>
+        <span className="frame1-text62">
+          <span>All Clinics</span>
+        </span>
+        <div className="frame1-group1406">
+          <div className="frame1-group471">
+            <span className="frame1-text64">
+              <span>Consult Online</span>
+            </span>
+          </div>
+        </div>
+        <img
+          alt="Vector183"
+          src="/playground_assets/vector183-en5c.svg"
+          className="frame1-vector2"
+        />
+        <div className="frame1-group1811">
+          <div className="frame1-group1789">
+            <span className="frame1-text66">
+              <span>Aurangabad</span>
+            </span>
+          </div>
+          <img
+            alt="Vector187"
+            src="/playground_assets/vector187-lyi.svg"
+            className="frame1-vector3"
+          />
+          <div className="frame1-group1786">
+            <div className="frame1-group4711">
+              <span className="frame1-text68">
+                <span>Contact</span>
+              </span>
+            </div>
+          </div>
+          <img
+            alt="Vector224191"
+            src="/playground_assets/vector224191-fcxa.svg"
+            className="frame1-vector2241"
+          />
+          <img
+            alt="Vector225192"
+            src="/playground_assets/vector225192-hc8dk.svg"
+            className="frame1-vector2251"
+          />
+          <div className="frame1-group">
+            <img
+              alt="Vector194"
+              src="/playground_assets/vector194-3xf8.svg"
+              className="frame1-vector4"
+            />
+            <img
+              alt="Vector195"
+              src="/playground_assets/vector195-4g4s.svg"
+              className="frame1-vector5"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Frame1
