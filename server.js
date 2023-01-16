7fee5a1745ae177ad23cc2b4adcef294996e961a
Pratik Pansare <pratik.p@qupapp.com>
2023-01-16 13:29:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 //server.js
-
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
@@ -10,9 +9,9 @@ app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
-    return res.send('pong');
+ return res.send('pong');
 });
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.js'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
